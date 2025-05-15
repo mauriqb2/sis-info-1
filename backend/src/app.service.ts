0b36aa769d738014aff0c96c79f0d8eb94552e1c
Mauricio Quezada <mauriqb2@gmail.com>
2025-05-15 19:49:10 -0400

    Implementando typeorm

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      nombre: "Pepe perales",
+      rol: 'Doctor',
+      ci: 8016331
+    };
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppDataSource } from './data-source';
+
+AppDataSource.initialize()
+    .then(() => {
+        // here you can start to work with your database
+    })
+    .catch((error) => console.log(error))
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
